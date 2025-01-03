f148c3629d372086f564f234aa6c77862a6a6c90
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-04 01:30:17 +0300

    changes

--- src/app/utils/logger.ts
+++ src/app/utils/logger.ts
@@ -9,16 +9,17 @@ const logger = createLogger({
   ),
 });
 
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  );
-}
+logger.add(
+  new transports.Console({
+    format: format.combine(format.colorize(), format.simple()),
+  })
+);
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(
+//     new transports.Console({
+//       format: format.combine(format.colorize(), format.simple()),
+//     })
+//   );
+// }
 
 export default logger;
