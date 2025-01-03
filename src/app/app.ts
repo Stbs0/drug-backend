05973546fc7ae60070a0f03973dc8586d0cd7b35
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-04 01:36:21 +0300

    cganges

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 import helmet from 'helmet';
 
-import { errorHandler, requestLogger, unknownEndpoint } from './utils/middleware.js';
 import api from './routes/route.js';
+import { errorHandler, requestLogger, unknownEndpoint } from './utils/middleware.js';
 
 const app = express();
 app.use(express.json());
@@ -12,7 +12,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-
+app.get('/health', (_req, res) => {
+  res.status(200).send('ok');
+});
 app.use('/api/v1', api);
 
 app.get('/ping', (_req, res) => {
