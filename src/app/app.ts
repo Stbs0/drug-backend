dc7d4bae02d6cf461691b2747cd9764ba2302f69
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-04 04:27:18 +0300

    s

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,8 @@ import helmet from 'helmet';
 
 import api from './routes/route.js';
 import { errorHandler, requestLogger, unknownEndpoint } from './utils/middleware.js';
-
+process.on('uncaughtException', (err) => console.error('Uncaught Exception:', err));
+process.on('unhandledRejection', (reason, _promise) => console.error('Unhandled Rejection:', reason));
 const app = express();
 app.use(express.json());
 
